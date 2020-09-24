25fcb1dc5e838581b02218e742fa2277a02dd894
diegoveigass <sdiegoveiga@gmail.com>
2020-09-24 18:41:09 -0300

    eslint, prettier, editorconfig

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function App() {
-  return <div className="App">Hello Xmobots</div>;
+  return (
+    <div>
+      <h1>Xmobots</h1>
+    </div>
+  );
 }
 
 export default App;
