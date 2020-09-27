abe1527ef1861f0669cf24963f22e93e21183134
diegoveigass <sdiegoveiga@gmail.com>
2020-09-27 17:22:48 -0300

    Fix: função parseDSMToLatLong

--- src/utils/parseDMSToLatLong.js
+++ src/utils/parseDMSToLatLong.js
@@ -3,7 +3,7 @@ function DMSToLatLong(hour, minutes, seconds, indicator) {
   const minutesNumber = Number(minutes);
   const secondsNumber = Number(seconds);
 
-  const decimal = hourNumber + (minutesNumber / 60 + secondsNumber) / 3600;
+  const decimal = hourNumber + (minutesNumber * 60 + secondsNumber) / 3600;
 
   if (indicator === 'S' || indicator === 'W') {
     return -decimal;
@@ -15,7 +15,7 @@ export function parseLatLong(dms) {
   const latReg = /0?(\d{2})(\d{2})(\d{2}([,.]\d{2})?)([S])/i;
   const latResult = latReg.exec(dms);
 
-  const latValue = DMSToLatLong(
+  const lat = DMSToLatLong(
     latResult[1],
     latResult[2],
     latResult[3].replace(',', '.'),
@@ -25,12 +25,12 @@ export function parseLatLong(dms) {
   const longReg = /0?(\d{2})(\d{2})(\d{2}([,.]\d{2})?)([W])/i;
   const longResult = longReg.exec(dms);
 
-  const longValue = DMSToLatLong(
+  const long = DMSToLatLong(
     longResult[1],
     longResult[2],
     longResult[3].replace(',', '.'),
-    latResult[5],
+    longResult[5],
   );
 
-  return [latValue, longValue];
+  return [lat, long];
 }
